dff888505d64954a543909ebea18a94a48133368
giovannifranco1 <giovannifrancorezende2012@gmail.com>
2022-03-25 00:48:44 -0300

    Implementação funções base FuncionarioRepository + Criação FuncionarioRepositoryInteface

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,13 +2,18 @@
 
 namespace App\Providers;
 
+use App\Repositories\FuncionarioRepository;
+use App\Repositories\Interfaces\FuncionarioRepositoryInterface;
+use App\Repositories\Interfaces\MovimentacaoRepositoryInterface;
+use App\Repositories\MovimentacaoRepository;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
   # Array repositories
   protected $repositories = [
-    //
+    FuncionarioRepositoryInterface::class => FuncionarioRepository::class,
+    MovimentacaoRepositoryInterface::class => MovimentacaoRepository::class,
   ];
   /**
    * Register services.
--- app/Repositories/FuncionarioRepository.php
+++ app/Repositories/FuncionarioRepository.php
@@ -2,12 +2,43 @@
 
 namespace App\Repositories;
 
+use App\Models\Funcionario;
 use App\Repositories\Interfaces\FuncionarioRepositoryInterface;
+use Illuminate\Contracts\Pagination\Paginator;
 
-class FuncionarioRepository implements FuncionarioRepositoryInterface
+class FuncionarioRepository extends BaseEloquentRepository implements FuncionarioRepositoryInterface
 {
-    public function __construct()
-    {
-        //
-    }
+  protected $model = Funcionario::class;
+  private $funcionarioEloquent;
+
+  public function __construct(Funcionario $funcionario)
+  {
+    $this->funcionarioEloquent = $funcionario;
+
+  }
+
+  public function criarFuncionario(Funcionario $funcionario): Funcionario
+  {
+    return $this->funcionarioEloquent
+      ->create($funcionario);
+  }
+
+  public function alterarFuncionario(Funcionario $funcionario, int $id)
+  {
+    return $this->funcionarioEloquent
+      ->find($id)
+      ->fill($funcionario)
+      ->save();
+  }
+
+  public function listarTodos(): Paginator
+  {
+    return $this->paginate('nome_completo', array('adiminstrator'));
+  }
+  public function excluirFuncionario(int $id)
+  {
+    return $this->funcionarioEloquent
+      ->find($id)
+      ->delete();
+  }
 }
